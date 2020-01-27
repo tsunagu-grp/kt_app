7baa5c8a6b66a882358d8d7d799ca8be2e7528d9
skawadu <skawatsu@geek.co.jp>
2020-01-27 15:35:38 +0900

    feat: `container`と`component`のベースを作成

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import ShowClock from '../containers/ShowClock'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      Clock！！！！
-    </div>
+    <ShowClock />
   );
 }
 
