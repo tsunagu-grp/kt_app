24c3a15c6faa3498ea73d86eae8116183e800341
skawadu <skawatsu@geek.co.jp>
2020-02-25 15:31:19 +0900

    update: 時計の更新ロジック

--- src/containers/ShowClock.tsx
+++ src/containers/ShowClock.tsx
@@ -3,11 +3,16 @@ import Clock from '../components/Clock'
 
 const ShowClock: React.FC = () => {
   const [date, setDate] = useState(new Date());
+  const nextTiming = 1000 - (Date.now() % 1000);
 
-  useEffect(()=>{
-    setInterval(()=>{setDate(new Date());
-    },1000)
-  },[date])
+  useEffect(() => {
+    let timer = setTimeout(() => {
+      setDate(new Date());
+    }, nextTiming);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [date, nextTiming]);
 
   return (
     <Clock year={date.getFullYear()} month={date.getMonth()+1} day={date.getDate()} hour={date.getHours()} minute={date.getMinutes()} second={date.getSeconds()} weekDay={date.getDay()}  />
